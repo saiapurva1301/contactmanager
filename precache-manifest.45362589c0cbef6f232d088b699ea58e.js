4e98cb3766c27a1653c18c31d8913d70a5ab521f
unknown <saiapurva1301@gmail.com>
2020-04-25 12:48:47 +0530

    Updates

---
+++ precache-manifest.45362589c0cbef6f232d088b699ea58e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ffd7b826aa46d5ec21725edf871d85",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1d32e0e5b9f52620b301",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2f393e33f6e0cd2018e4",
+    "url": "/contactmanager/static/css/main.f8ae64f1.chunk.css"
+  },
+  {
+    "revision": "1d32e0e5b9f52620b301",
+    "url": "/contactmanager/static/js/2.c1e02624.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.c1e02624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f393e33f6e0cd2018e4",
+    "url": "/contactmanager/static/js/main.041f3a6a.chunk.js"
+  },
+  {
+    "revision": "24a7aa86c223723edee6",
+    "url": "/contactmanager/static/js/runtime-main.0453592f.js"
+  }
+]);
