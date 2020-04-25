3be25f3cacb4d260feaa20e9b6ca9dd71f25de9b
unknown <saiapurva1301@gmail.com>
2020-04-25 13:04:26 +0530

    Updates

--- precache-manifest.45362589c0cbef6f232d088b699ea58e.js
+++ precache-manifest.45362589c0cbef6f232d088b699ea58e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b6d9092a5d00a2968604e63feb251d6f",
+    "revision": "26ffd7b826aa46d5ec21725edf871d85",
     "url": "/contactmanager/index.html"
   },
   {
-    "revision": "a9c6b1b35c8d774dc83f",
+    "revision": "1d32e0e5b9f52620b301",
     "url": "/contactmanager/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "017550046d1258580c4d",
+    "revision": "2f393e33f6e0cd2018e4",
     "url": "/contactmanager/static/css/main.f8ae64f1.chunk.css"
   },
   {
-    "revision": "a9c6b1b35c8d774dc83f",
-    "url": "/contactmanager/static/js/2.226e7271.chunk.js"
+    "revision": "1d32e0e5b9f52620b301",
+    "url": "/contactmanager/static/js/2.c1e02624.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/contactmanager/static/js/2.226e7271.chunk.js.LICENSE.txt"
+    "url": "/contactmanager/static/js/2.c1e02624.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "017550046d1258580c4d",
-    "url": "/contactmanager/static/js/main.66f08b20.chunk.js"
+    "revision": "2f393e33f6e0cd2018e4",
+    "url": "/contactmanager/static/js/main.041f3a6a.chunk.js"
   },
   {
     "revision": "24a7aa86c223723edee6",
